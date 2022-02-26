609f53619c417accbb7d3069c72d5fc28ecdd950
Luke <lukethegreat000@gmail.com>
2022-02-25 21:26:55 -0800

    finished p3 everything is working as expected from the instructions.

---
+++ p3/video.cpp
@@ -0,0 +1,35 @@
+// video.cpp
+// Cordova, Luke
+#include <iostream>
+#include "video.h"
+using namespace std;
+
+
+Video::Video(string title, string URL, string comment, float length, int rating){
+    m_title = title;
+    m_URL = URL;
+    m_comment = comment;
+    m_length = length;
+    m_rating = rating;
+}
+void Video::print(){
+    cout << m_title << ", " << m_URL << ", ";
+    cout << m_comment << ", " << m_length << ", ";
+    for(int i = 0;i<m_rating;i++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+bool Video::byRating(Video *other){
+    return m_rating < other->m_rating;
+}
+bool Video::byTitle(Video *other){
+    return m_title > other->m_title;
+}
+bool Video::byLength(Video *other){
+    return m_length > other->m_length;
+}
+string Video::getTitle(){
+    return m_title;
+}
---
+++ p3/video.h
@@ -0,0 +1,25 @@
+// video.h
+// Cordova, Luke
+
+#ifndef VIDEO_H
+#define VIDEO_H
+#include <iostream>
+using namespace std;
+
+class Video{
+    public:
+        Video();
+        Video(string title, string URL, string comment, float length, int rating);
+        void print();
+        bool byLength(Video *other);
+        bool byTitle(Video *other);
+        bool byRating(Video *other);
+        string getTitle();
+    private:
+        string m_title;
+        string m_URL;
+        string m_comment;
+        float m_length;
+        int m_rating;
+};
+#endif
