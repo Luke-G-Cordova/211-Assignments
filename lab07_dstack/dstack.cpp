e50f7daded9fe5c3832ebd046780f8ff4ef8eefc
Luke <lukethegreat000@gmail.com>
2022-02-26 14:43:37 -0800

    lab07 done.

---
+++ lab07_dstack/dstack.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include "dstack.h"
+using namespace std;
+
+Dstack::~Dstack(){
+    Node* delete_me = m_head;
+    while(m_head!=NULL){
+        m_head = m_head->m_next;
+        delete delete_me;
+        delete_me = m_head;
+    }
+}
+
+void Dstack::push(double value){
+    m_head = new Node(value, m_head);
+    m_length++;
+}
+
+bool Dstack::pop(double &value){
+    if(m_head == NULL)return false;
+    value = m_head->m_value;
+    Node* delete_me = m_head;
+    m_head = m_head->m_next;
+    delete delete_me;
+    m_length--;
+    return true;
+}
+
+int Dstack::size(){
+    return m_length;
+}
+bool Dstack::empty(){
+    return m_head == NULL;
+}
